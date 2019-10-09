ebf9e82f81abd6f21b2bae6055157affa45964f8
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-09 23:53:41 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import React, { FC, useState } from "react";
 
-import React from "react";
+import Modal from "./Modal";
+import styled from "styled-components";
 
-const App: React.FC = () => {
+const App: FC = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Base>
-      <Modal>
+    <div>
+      <button onClick={() => setOpen(p => !p)}>open</button>
+      <Modal open={open} onClose={() => setOpen(false)}>
         <Card>
           <p>title</p>
-          <button onClick={() => console.log("button")}>click</button>
+          <button onClick={() => setOpen(false)}>click</button>
         </Card>
       </Modal>
-      <Backdrop onClick={() => console.log("backdrop")}></Backdrop>
-    </Base>
+    </div>
   );
 };
-
-const fadeIn = keyframes`
-from {
-  opacity:0
-  scale:0.8
-}
-to {
-  opacity:1
-  scale:1
-}
-`;
-
-const fadeOut = keyframes`
-from {
-  opacity:1
-  scale:1
-}
-to {
-  opacity:0
-  scale:0.8
-}
-`;
-
-const Base = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 2;
-  width: 500px;
-`;
-const Backdrop = styled.div`
-  position: fixed;
-  z-index: 1;
-  height: 100vh;
-  width: 100vw;
-  background: black;
-  opacity: 0.7;
-  animation: ${fadeIn} 0.1s ease-in;
-`;
-
-const Modal = styled.div`
-  z-index: 2;
-  width: 100%;
-  background-color: white;
-  animation: ${fadeIn} 0.3s ease-in;
-`;
 
 const Card = styled.div`
   padding: 20px;
---
+++ src/Modal.tsx
@@ -0,0 +1,79 @@
+import React, { Fragment, useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+const Modal: React.FC<{
+  open: boolean;
+  minWidth?: number;
+  onClose?: () => void;
+}> = ({ minWidth, children, open, onClose }) => {
+  const [opened, setOpened] = useState(open);
+
+  useEffect(() => {
+    open && setOpened(true);
+  }, [open]);
+
+  return (
+    <Fragment>
+      {(opened || open) && (
+        <Base open={open} minWidth={minWidth}>
+          <Container open={open}>{children}</Container>
+          <Backdrop open={open} onClick={onClose}></Backdrop>
+        </Base>
+      )}
+    </Fragment>
+  );
+};
+
+const fadeIn = keyframes`
+from {
+  opacity:0
+  scale:0.8
+}
+to {
+  opacity: 0.7;
+  scale:1
+}
+`;
+
+const fadeOut = keyframes`
+from {
+  opacity: 0.7;
+  scale:1
+}
+to {
+  opacity:0
+  scale:0.8
+  visibility:hidden
+}
+`;
+
+const Base = styled.div<{ open: boolean; minWidth?: number }>`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: ${({ minWidth }) => minWidth || 400}px;
+  animation: ${({ open }) => (open ? fadeIn : fadeOut)} 0.1s ease-in forwards;
+`;
+const Backdrop = styled.div<{ open: boolean }>`
+  position: fixed;
+  z-index: 1;
+  height: 100vh;
+  width: 100vw;
+  background: black;
+  opacity: 0.7;
+  animation: ${({ open }) => (open ? fadeIn : fadeOut)} 0.1s ease-in;
+`;
+
+const Container = styled.div<{ open: boolean }>`
+  z-index: 2;
+  width: 100%;
+  background-color: white;
+  animation: ${({ open }) => (open ? fadeIn : fadeOut)} 0.3s ease-in;
+`;
+
+export default Modal;
